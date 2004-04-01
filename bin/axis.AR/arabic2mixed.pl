3d31ea4a64284c271e870fe9a85c2d4bccb91425
Xiaoyi Ma <xma@ldc.upenn.edu>
2004-04-01 19:51:50 +0000

    *** empty log message ***

--- bin/axis.AR/arabic2mixed.pl
+++ bin/axis.AR/arabic2mixed.pl
@@ -1,38 +1,27 @@
-#!/home/xma/pkg/bin/perl
+#!/usr/bin/perl -p
+#
+# Purpose: convert Arabic-Indic digits and Arabic
+#          punctuations to English equivalents
+#
 
-$Usage = "Usage: $0 [Arabic .txt file (UTF8)]";
+# convert Arabic-Indic digits to Roman digits
+s/\xD9\xA0|\xDB\xB0/0/g;
+s/\xD9\xA1|\xDB\xB1/1/g;
+s/\xD9\xA2|\xDB\xB2/2/g;
+s/\xD9\xA3|\xDB\xB3/3/g;
+s/\xD9\xA4|\xDB\xB4/4/g;
+s/\xD9\xA5|\xDB\xB5/5/g;
+s/\xD9\xA6|\xDB\xB6/6/g;
+s/\xD9\xA7|\xDB\xB7/7/g;
+s/\xD9\xA8|\xDB\xB8/8/g;
+s/\xD9\xA9|\xDB\xB9/9/g;
 
-use Encode;
+# convert Arabic punctuations to English puctuations
+s/\xD8\x9F/\?/g;
+s/\xD9\xAA/\%/g;
+s/\xD9\xAB/\./g; # Arabic decimal separator
+s/\xD9\xAC/\,/g; # Arabic thousand separator
+s/\xD8\x9B/\;/g;
+s/\xD8\x8C/\,/g;
 
-$Usage = "Usage: $0 [filename]\n";
-die $Usage if ( @ARGV > 1 or ( @ARGV == 1 and ! -f $ARGV[0] ));
 
-if ( @ARGV ) {
-    open( STDIN, shift );
-}
-binmode STDIN, ":utf8";
-binmode STDOUT, ":utf8";
-
-while(<>){
-    
-    # convert Arabic-Indic digits to Roman digits
-    s/\x{0660}|\x{06F0}/0/g;
-    s/\x{0661}|\x{06F1}/1/g;
-    s/\x{0662}|\x{06F2}/2/g;
-    s/\x{0663}|\x{06F3}/3/g;
-    s/\x{0664}|\x{06F4}/4/g;
-    s/\x{0665}|\x{06F5}/5/g;
-    s/\x{0666}|\x{06F6}/6/g;
-    s/\x{0667}|\x{06F7}/7/g;
-    s/\x{0668}|\x{06F8}/8/g;
-    s/\x{0669}|\x{06F9}/9/g;
-
-    # convert Arabic punctuations to English puctuations
-    s/\x{061F}/\?/g;
-    s/\x{066A}/\%/g;
-    s/\x{066B}/\./g; # Arabic decimal separator
-    s/\x{066C}/\,/g; # Arabic thousand separator
-    s/\x{061B}/\;/g;
-    s/\x{060C}/\,/g;
-    print;
-}
